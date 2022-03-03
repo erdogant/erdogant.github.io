e1947070c19af6424b0e8f3d8aa6de6ccad4bd5b
erdogant <erdogant@gmail.com>
2022-03-03 14:12:11 +0100

    update carbon ad script

---
+++ carbon_ads/carbon_ads.js
@@ -0,0 +1,7 @@
+<script>
+!function() {
+<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CEADP27U&placement=erdogantgithubio" id="_carbonads_js"></script>
+}
+</script>
+
+
