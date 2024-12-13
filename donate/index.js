6b06025a15166b2f7d022685e8fbcfcf7f3af80b
erdogant <erdogant@gmail.com>
2024-12-13 14:00:37 +0100

    update coffee moments

--- donate/index.js
+++ donate/index.js
@@ -1,6 +1,6 @@
 // donate-bitcoin Copyright (GPL) 2016  Nathan Robinson
 
-var address = "3QkpLdvCKfkZftCUXHB1LDDYQvquZCfM8x"; // The bitcoin address to receive donations. Change to yours
+var address = ""; // The bitcoin address to receive donations. Change to yours
 var popup = true; // Set to true if you want a popup to pay bitcoin
 var currencyCode = "USD"; // Change to your default currency. Choose from https://blockchain.info/ticker?cors=true
 var qrcode = true; // Set to false to disable qrcode
